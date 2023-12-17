ec941216ffa506c1d9b924b4b720de7d30696b1e
IlayFurman2025 <if6192@princeton.edu>
2023-12-16 23:24:07 -0500

    data masking partially works

--- content_scripts/data_masker.js
+++ content_scripts/data_masker.js
@@ -1,31 +1,84 @@
 let isMaskingEnabled = false;
 
-// Function to mask data
-function maskData() {
-  const inputFields = document.querySelectorAll('input[type="text"]');
-  inputFields.forEach((field) => {
-    if (!field.dataset.originalValue) {
+// Function to mask data in a field
+function maskData(field) {
+  if (field.dataset.originalValue === undefined) {
+    field.dataset.originalValue = field.value;
+  } else {
+    // Update the stored original value only if it's not currently masked
+    if (!isMaskingEnabled) {
       field.dataset.originalValue = field.value;
-      field.value = field.value.replace(/./g, "*"); // Replace with asterisks or other characters
     }
+  }
+  if (isMaskingEnabled) {
+    field.value = field.value.replace(/./g, "*");
+  }
+}
+
+// Function to unmask data in a field
+function unmaskData(field) {
+  if (field.dataset.originalValue !== undefined) {
+    field.value = field.dataset.originalValue;
+  }
+}
+
+// Apply or remove masking from a field based on isMaskingEnabled
+function applyMaskingToField(field) {
+  if (isMaskingEnabled) {
+    maskData(field);
+  } else {
+    unmaskData(field);
+  }
+}
+
+// Set up event listeners on input fields for real-time masking and value updates
+function setupFieldListeners(field) {
+  field.addEventListener("input", () => {
+    maskData(field);
   });
 }
 
-// Function to unmask data
-function unmaskData() {
-  const inputFields = document.querySelectorAll('input[type="text"]');
+// Iterate over and initialize all applicable input fields
+function initializeInputFields() {
+  const inputFields = document.querySelectorAll(
+    'input[type="text"], input[type="search"], input:not([type])'
+  );
   inputFields.forEach((field) => {
-    if (field.dataset.originalValue) {
-      field.value = field.dataset.originalValue;
-      delete field.dataset.originalValue;
-    }
+    applyMaskingToField(field);
+    setupFieldListeners(field);
   });
 }
+
+// MutationObserver to monitor changes in the DOM and apply masking to new input fields
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (node.querySelectorAll) {
+        node
+          .querySelectorAll(
+            'input[type="text"], input[type="search"], input:not([type])'
+          )
+          .forEach((field) => {
+            applyMaskingToField(field);
+            setupFieldListeners(field);
+          });
+      }
+    });
+  });
+});
+
+// Start observing the document body for added nodes
+observer.observe(document.body, { childList: true, subtree: true });
 
 // Listen for toggle message from popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.toggleMasking !== undefined) {
     isMaskingEnabled = request.toggleMasking;
-    isMaskingEnabled ? maskData() : unmaskData();
+    initializeInputFields();
   }
+});
+
+// Initialize fields on DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+  initializeInputFields();
 });
