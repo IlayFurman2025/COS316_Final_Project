c0297d2d6120245f7894134854b304e136a3482d
IlayFurman2025 <if6192@princeton.edu>
2023-12-18 22:26:35 +0200

    works

--- background.js
+++ background.js
@@ -4,19 +4,16 @@ chrome.runtime.onInstalled.addListener(function () {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.adBlockerEnabled !== undefined) {
-    // Update ad blocking functionality based on the state
     updateAdBlocking(request.adBlockerEnabled);
   }
 });
 
 function updateAdBlocking(enabled) {
   if (enabled) {
-    // Enable the entire ruleset defined in rules.json
     chrome.declarativeNetRequest.updateEnabledRulesets({
       enableRulesetIds: ["ruleset_1"],
     });
   } else {
-    // Disable the entire ruleset
     chrome.declarativeNetRequest.updateEnabledRulesets({
       disableRulesetIds: ["ruleset_1"],
     });
